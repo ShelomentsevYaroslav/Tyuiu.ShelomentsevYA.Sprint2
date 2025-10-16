bd5f5fcc6d76f6455ff4846aa643d28747071b97
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-16 23:36:23 +0500

    Fix

--- Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Lib/DataService.cs
+++ Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Lib/DataService.cs
@@ -4,11 +4,9 @@ namespace Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Lib
 {
     public class DataService : ISprint2Task6V9
     {
-
         public string FindDateOfNextDay(int m, int n)
         {
             int daysInMonth = 0;
-
 
             switch (m)
             {
@@ -21,14 +19,12 @@ namespace Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Lib
                 case 12:
                     daysInMonth = 31;
                     break;
-
                 case 4:
                 case 6:
                 case 9:
                 case 11:
                     daysInMonth = 30;
                     break;
-
                 case 2:
                     daysInMonth = 28;
                     break;
@@ -37,21 +33,18 @@ namespace Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Lib
             int nextDay = n + 1;
             int nextMonth = m;
 
-
             if (nextDay > daysInMonth)
             {
                 nextDay = 1;
                 nextMonth++;
             }
 
-
             if (nextMonth > 12)
             {
                 nextMonth = 1;
             }
 
- 
-            return $"{nextDay}.{nextMonth}";
+            return $"{nextDay:D2}.{nextMonth:D2}";
         }
     }
 }
