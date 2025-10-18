343fa3b8ae30a5585bdd67c3642289969b2613fa
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-18 07:37:09 +0500

    Fix Test Assert

--- Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Test/DataServiceTest.cs
+++ Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Test/DataServiceTest.cs
@@ -11,11 +11,11 @@ namespace Tyuiu.ShelomentsevYA.Sprint2.Task6.V9.Test
         {
             DataService ds = new DataService();
 
-            Assert.AreEqual("2.1", ds.FindDateOfNextDay(1, 1));
-            Assert.AreEqual("1.2", ds.FindDateOfNextDay(1, 31));
-            Assert.AreEqual("1.3", ds.FindDateOfNextDay(2, 28));
-            Assert.AreEqual("1.5", ds.FindDateOfNextDay(4, 30));
-            Assert.AreEqual("1.1", ds.FindDateOfNextDay(12, 31));
+            Assert.AreEqual("02.01", ds.FindDateOfNextDay(1, 1));
+            Assert.AreEqual("01.02", ds.FindDateOfNextDay(1, 31));
+            Assert.AreEqual("01.03", ds.FindDateOfNextDay(2, 28));
+            Assert.AreEqual("01.05", ds.FindDateOfNextDay(4, 30));
+            Assert.AreEqual("01.01", ds.FindDateOfNextDay(12, 31));
         }
     }
 }
